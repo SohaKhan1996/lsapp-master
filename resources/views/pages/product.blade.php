3b5891d1caf936e9ca64a25afc7bd309e6b9c83e
Rizwana Khan <rizwanakhan4522@gmail.com>
2018-11-22 19:05:03 +0500

    Revert "comment out irrelevant part"
    This reverts commit a12e8b6a3b5ed67889c04983b2b60f61e01ea53a.

--- resources/views/pages/product.blade.php
+++ resources/views/pages/product.blade.php
@@ -242,7 +242,7 @@
 										<div class="block2-btn-addcart w-size1 trans-0-4">
 										<form action="{{route('cart.store')}}" method= "POST">
 										  {{csrf_field()}}
-										  <!-- <input type="hidden " name="id" value="{{$product->id}}">
+										   <input type="hidden " name="id" value="{{$product->id}}">
 										   <input type="hidden " name="name" value="{{$product->name}}">
 										   <input type="hidden " name="price" value="{{$product->price}}">
 											<!-- Button -->
