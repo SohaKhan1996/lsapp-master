d216e854ac1a16e49a8a18979aa854ab68c250e3
sohakhan <sohakhan@khi.iba.edu.pk>
2018-11-22 16:01:40 +0500

    Revert "cart part -1"
    This reverts commit c5acfcf009eae09fd01f77a25dfa4639ca5c0820.

--- app/Product.php
+++ app/Product.php
@@ -6,10 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-     public function presentPrice(){
-         return money_format('$%i', $this->price / 100);
-     }
-     function formatcurrency($floatcurr, $curr = "USD"){
-     $currencies['PKR'] = array(2,'.',','); 
-     }         //  Pakistan Rupee
+    // public function presentPrice(){
+    //     return money_format('$%i', $this->price / 100);
+    // }
+    // function formatcurrency($floatcurr, $curr = "USD"){
+    // $currencies['PKR'] = array(2,'.',','); 
+    // }         //  Pakistan Rupee
 }
